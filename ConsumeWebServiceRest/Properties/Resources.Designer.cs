08b2ce58d12fd782eec2af0f4a2e78c877b26ff5
Ibrahim <ibrahim.duman@2isa.org>
2016-11-28 14:48:37 +0100

    Ajout de la classe ConsumeWSR

--- ConsumeWebServiceRest/Properties/Resources.Designer.cs
+++ ConsumeWebServiceRest/Properties/Resources.Designer.cs
@@ -64,9 +64,63 @@ namespace ConsumeWebServiceRest.Properties {
         /// <summary>
         ///   Recherche une chaîne localisée semblable à .
         /// </summary>
+        internal static string ERREUR_APPELSERVICE {
+            get {
+                return ResourceManager.GetString("ERREUR_APPELSERVICE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à .
+        /// </summary>
+        internal static string ERREUR_DESERIALISATIONRETOUR {
+            get {
+                return ResourceManager.GetString("ERREUR_DESERIALISATIONRETOUR", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à .
+        /// </summary>
+        internal static string ERREUR_SERIALISATIONPARAMS {
+            get {
+                return ResourceManager.GetString("ERREUR_SERIALISATIONPARAMS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à .
+        /// </summary>
         internal static string ERREUR_SERIALISATIONRESULT {
             get {
                 return ResourceManager.GetString("ERREUR_SERIALISATIONRESULT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à .
+        /// </summary>
+        internal static string ERREUR_TIMEOUT {
+            get {
+                return ResourceManager.GetString("ERREUR_TIMEOUT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à .
+        /// </summary>
+        internal static string SERIALISATION_JSON {
+            get {
+                return ResourceManager.GetString("SERIALISATION_JSON", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à .
+        /// </summary>
+        internal static string SERIALISATION_XML {
+            get {
+                return ResourceManager.GetString("SERIALISATION_XML", resourceCulture);
             }
         }
     }
