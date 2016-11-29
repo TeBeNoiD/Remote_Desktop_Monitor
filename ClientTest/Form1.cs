73f3b8a6e0448f10ba8839f90b044769fd3e776a
SebDev2i <sebastien.nosjean@2isa.org>
2016-11-29 17:44:12 +0100

    modif winform

--- ClientTest/Form1.cs
+++ ClientTest/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Timers;
 
 namespace ClientTest
 {
@@ -18,8 +19,19 @@ namespace ClientTest
         public frmClient()
         {
             InitializeComponent();
+            
         }
 
+        private async void StartTimer(WSR_Params p)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            WSR_Result r = await ConsumeWSR.Call(@"http://localhost:4000/Service.svc/GetPseudos", p, TypeSerializer.Json, _CancellationAsync.Token);
+            List<string> lst = new List<string>();
+            lst = (List<string>)r.Data;
+            lstPseudo.DataSource = lst;
+            timer.Start();
+        }
         private async void cmdCnx_Click(object sender, EventArgs e)
         {
             _CancellationAsync = new CancellationTokenSource();
@@ -28,13 +40,12 @@ namespace ClientTest
             WSR_Result r1 = await ConsumeWSR.Call(@"http://localhost:4000/Service.svc/Login", p, TypeSerializer.Json, _CancellationAsync.Token);
             lblPwd.Text = (string)r1.Data;
             p.Add("password", (string)r1.Data);
-            WSR_Result r2 = await ConsumeWSR.Call(@"http://localhost:4000/Service.svc/GetPseudos", p, TypeSerializer.Json, _CancellationAsync.Token);
+            StartTimer(p);
+            /*WSR_Result r2 = await ConsumeWSR.Call(@"http://localhost:4000/Service.svc/GetPseudos", p, TypeSerializer.Json, _CancellationAsync.Token);
 
             List<string> lst = new List<string>();
             lst = (List<string>)r2.Data;
-            lstPseudo.DataSource = lst;
-
-            //Console.WriteLine("mot de passe : " + r.Data);
+            lstPseudo.DataSource = lst;*/
         }
 
         private async void cmdDecnx_Click(object sender, EventArgs e)
