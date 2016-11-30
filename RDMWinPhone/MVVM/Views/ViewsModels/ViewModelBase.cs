5d01adfed1e372668de4a21f43e1e85ff5d82b81
Ibrahim <ibrahim.duman@2isa.org>
2016-11-30 11:54:25 +0100

    CREATION DOSSIER MVVM / Creation du sous-dossier ViewsModels du dossier MVVM Création des classes MonitorViewModel, UserViewModel, ViewModelBase

---
+++ RDMWinPhone/MVVM/Views/ViewsModels/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RDMWinPhone.MVVM.Views.ViewsModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Membres
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Méthodes
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion Méthodes
+    }
+}
